41eeb1642c38e9abcad518129491807599a3a24c
cdw79 <canxiong818@gmail.com>
2015-05-24 20:48:19 -0400

    Set icon for food truck locations, click on location marker change color and show related info window

--- map.js
+++ map.js
@@ -1,3 +1,6 @@
+var markersArray = []; // global markers containers
+var selectedMarkerId = 0;
+
 $(window).load(function() {
 	var mapOptions = {
 		center: new google.maps.LatLng(37.7577, -122.4376),
@@ -26,6 +29,8 @@ $(window).load(function() {
 	google.maps.event.addListener(autocomplete, 'place_changed', function() {
 		infowindow.close();
 		marker.setVisible(false);
+		clearMarkers();
+		
 		var place = autocomplete.getPlace();
 		if (!place.geometry) {
 			window.alert("Autocomplete's returned place contains no geometry");
@@ -58,16 +63,13 @@ $(window).load(function() {
 		infowindow.open(map, marker);
 
 
-		setMarkers(map, infowindow);
-
-
+		setMarkers(map, infowindow, place);
+		
 	});
 
 
 	// Sets a listener on a radio button to change the filter type on Places
 	// Autocomplete.
-
-
 	function setupClickListener(id, types) {
 		var radioButton = document.getElementById(id);
 		google.maps.event.addDomListener(radioButton, 'click', function() {
@@ -82,29 +84,79 @@ $(window).load(function() {
 
 });
 
-function setMarkers(map, infowindow) {
+// Check food truck location is within 20km around search location or not.
+function withinRange(entry, place) {
+	// console.log("entry: "+entry.location.latitude + ", " + entry.location.longitude + " --- place: " + place.geometry.location);
+	var p1 = new google.maps.LatLng(entry.location.latitude, entry.location.longitude);
+	var p2 = place.geometry.location;
+	//calculates distance between two points in km's
+	var distance = (google.maps.geometry.spherical.computeDistanceBetween(p1, p2) / 1000).toFixed(2);
+	
+	// console.log(distance);	
+	return distance <= 5;
+}
+
+function clearMarkers() {
+	for(var i=0; i<markersArray.length; i++) {
+		markersArray[i].setMap(null);
+	}
+	markersArray.length = 0;
+}
+
+function setMarkers(map, infowindow, place) {
+	var pinIcon = new google.maps.MarkerImage(
+	    "images/pin-1.png",
+	    null, /* size is determined at runtime */
+	    null, /* origin is 0,0 */
+	    null, /* anchor is bottom center of the scaled image */
+	    new google.maps.Size(20, 30)
+	);
+	
 	// Construct the catalog query string
 	url = 'https://data.sfgov.org/resource/rqzj-sfat.json';
 	// Retrieve our data and plot it
 	$.getJSON(url, function(data, textstatus) {
-		// console.log(data);
+		console.log(data);
+		var i = 0;
 		$.each(data, function(i, entry) {
-			var items = entry.fooditems.split(':').join(', ');
-			var marker = new google.maps.Marker({
-				position: new google.maps.LatLng(entry.location.latitude, entry.location.longitude),
-				animation: google.maps.Animation.DROP,
-				map: map,
-				title: location.name,
-				details: '<div><strong>' + entry.applicant + '</strong><br>' + entry.address + '<br><i>' + items + '</i>'
-			});
+			if(entry.location != undefined && entry.status != "EXPIRED" && withinRange(entry, place)) {
+				var items = entry.fooditems.split(':').join(', ');
+				var marker = new google.maps.Marker({
+					position: new google.maps.LatLng(entry.location.latitude, entry.location.longitude),
+					animation: google.maps.Animation.DROP,
+					icon: pinIcon,
+					map: map,
+					title: location.name,
+					details: '<div><strong>' + entry.applicant + '</strong><br>' + entry.address + '<br><i>' + items + '</i>'
+				});
+				markersArray[i++] = marker; // Add to global container for future clearance
 
-			google.maps.event.addListener(marker, 'click', function() {
-				infowindow.setContent(this.details);
-				infowindow.open(map, this);
-			});
-
+				google.maps.event.addListener(marker, 'click', function() {
+					changeIcon(pinIcon); // Switch marker from selected to unselect
+					pinIcon = new google.maps.MarkerImage(
+					    "images/pin-2.png",
+					    null, /* size is determined at runtime */
+					    null, /* origin is 0,0 */
+					    null, /* anchor is bottom center of the scaled image */
+					    new google.maps.Size(22, 32) //(37, 52)
+					);
+					marker.setIcon(pinIcon);
+					infowindow.setContent(this.details);
+					infowindow.open(map, this);
+					selectedMarkerId = i-1; // Remember previous selected marker id
+				});
+			}
 		});
 	});
-
 }
 
+function changeIcon(pinIcon) {
+	pinIcon = new google.maps.MarkerImage(
+	    "images/pin-1.png",
+	    null, /* size is determined at runtime */
+	    null, /* origin is 0,0 */
+	    null, /* anchor is bottom center of the scaled image */
+	    new google.maps.Size(20, 30) //(35, 50)
+	);
+	markersArray[selectedMarkerId].setIcon(pinIcon);
+}
