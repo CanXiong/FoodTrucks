6a79ab872d6f13e78a44466487d0eb3f8fc739a9
cdw79 <canxiong818@gmail.com>
2015-05-26 23:08:52 -0400

    When range value changed, map updated immediately. Make window load function more readable.

--- map.js
+++ map.js
@@ -2,14 +2,17 @@
 // Global Variables
 /*************************************************************/
 var map;
+var autocomplete;
+var marker, infowindow; // Current location marker and infowindow
 var markersArray = []; // global markers containers
 var selectedMarkerId = -1;
 var range = 1; // default search radius is 1 mile
 var place; // user placed location
 
-var directionsDisplay; // used for routing
-var directionsService = new google.maps.DirectionsService();
-var travelWay = google.maps.TravelMode.DRIVING;
+// used for routing
+var directionsDisplay; 
+var directionsService;
+var travelWay = google.maps.TravelMode.DRIVING; // default is driving
 
 /*************************************************************/
 // Window Load
@@ -22,7 +25,7 @@ $(window).load(function() {
 		zoom: 13
 	};
 	map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
+	directionsService = new google.maps.DirectionsService();
 	directionsDisplay = new google.maps.DirectionsRenderer();
 	directionsDisplay.setMap(map);
 
@@ -33,69 +36,29 @@ $(window).load(function() {
 	map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
 	map.controls[google.maps.ControlPosition.TOP_LEFT].push(types);
 
-	var autocomplete = new google.maps.places.Autocomplete(input);
+	autocomplete = new google.maps.places.Autocomplete(input);
 	autocomplete.bindTo('bounds', map);
 	//autocomplete.setTypes([]); // All includes Establishments, Addresses, Geocodes
-	var infowindow = new google.maps.InfoWindow({
+	infowindow = new google.maps.InfoWindow({
 		maxWidth: 200
 	});
-	var marker = new google.maps.Marker({
+	marker = new google.maps.Marker({
 		map: map,
 		anchorPoint: new google.maps.Point(0, -29)
 	});
 
 	google.maps.event.addListener(autocomplete, 'place_changed', function() {
-		infowindow.close();
-		marker.setVisible(false);
-		clearMarkers();
-		// Clear direction display
-		if (directionsDisplay != null) {
-			directionsDisplay.setMap(null);
-			directionsDisplay = null;
-		}
-		directionsDisplay = new google.maps.DirectionsRenderer();
-		directionsDisplay.setMap(map);
-
-		place = autocomplete.getPlace();
-		if (!place.geometry) {
-			window.alert("Autocomplete's returned place contains no geometry");
-			return;
-		}
-
-		// If the place has a geometry, then present it on a map.
-		if (place.geometry.viewport) {
-			map.fitBounds(place.geometry.viewport);
-		} else {
-			map.setCenter(place.geometry.location);
-			map.setZoom(16); // Why 17? Because it looks good.
-		}
-		marker.setIcon( /** @type {google.maps.Icon} */ ({
-			url: place.icon,
-			size: new google.maps.Size(71, 71),
-			origin: new google.maps.Point(0, 0),
-			anchor: new google.maps.Point(17, 34),
-			scaledSize: new google.maps.Size(35, 35)
-		}));
-		marker.setPosition(place.geometry.location);
-		marker.setVisible(true);
-
-		var address = '';
-		if (place.address_components) {
-			address = [
-			(place.address_components[0] && place.address_components[0].short_name || ''), 
-			(place.address_components[1] && place.address_components[1].short_name || ''), 
-			(place.address_components[2] && place.address_components[2].short_name || '')
-			].join(' ');
-		}
-		
-		// Display the user input location info window
-		infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
-		infowindow.open(map, marker);
-
-		// Found food trucks around you
-		setMarkers(infowindow);
+		autocompleteFunc();
 	});
 
+	radioSetUps();
+});
+
+/*************************************************************/
+// radio buttons setups
+// Set up four radio buttons to each corresponding listener function.
+/*************************************************************/
+function radioSetUps() {
 	setupClickListener('Car');
 	setupClickListener('Transit');
 	setupClickListener('Bicycle');
@@ -110,7 +73,110 @@ $(window).load(function() {
 			travelWay = google.maps.TravelMode[selectedMode];
 		});
 	}
-});
+}
+
+/*************************************************************/
+// autocomplete function
+// When autocomplete event is triggered, clean up the map view;
+// set the focus to the place; display the place infowidown;
+// and find nearby food trucks locations.
+/*************************************************************/
+function autocompleteFunc() {
+	infowindow.close();
+	marker.setVisible(false);
+	clearMarkers();
+	google.maps.event.trigger(map,'resize');
+	// Clear direction display
+	if (directionsDisplay != null) {
+		directionsDisplay.setMap(null);
+		directionsDisplay = null;
+	}
+	directionsDisplay = new google.maps.DirectionsRenderer();
+	directionsDisplay.setMap(map);
+
+	place = autocomplete.getPlace();
+	if (!place.geometry) {
+		window.alert("Autocomplete's returned place contains no geometry");
+		return;
+	}
+	
+	// If the place has a geometry, then present it on a map.
+	if (place.geometry.viewport) {
+		map.fitBounds(place.geometry.viewport);
+	} else {
+		map.setCenter(place.geometry.location);
+		map.setZoom(15); // Why 15? Because it looks good.
+	}
+	marker.setIcon( /** @type {google.maps.Icon} */ ({
+		url: place.icon,
+		size: new google.maps.Size(71, 71),
+		origin: new google.maps.Point(0, 0),
+		anchor: new google.maps.Point(17, 34),
+		scaledSize: new google.maps.Size(35, 35)
+	}));
+	marker.setPosition(place.geometry.location);
+	marker.setVisible(true);
+
+	var address = '';
+	if (place.address_components) {
+		address = [
+		(place.address_components[0] && place.address_components[0].short_name || ''), 
+		(place.address_components[1] && place.address_components[1].short_name || ''), 
+		(place.address_components[2] && place.address_components[2].short_name || '')
+		].join(' ');
+	}
+	
+	// Display the user input location info window
+	infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+	infowindow.open(map, marker);
+
+	// Found food trucks around you
+	setMarkers(infowindow);
+}
+
+/*************************************************************/
+// range updated
+// When range value changed, reset all the food trucks locations.
+// similar actions to autocomplete function above.
+/*************************************************************/
+function rangeUpdated() {
+	infowindow.close();
+	marker.setVisible(false);
+	clearMarkers();
+	google.maps.event.trigger(map,'resize');
+	// Clear direction display
+	if (directionsDisplay != null) {
+		directionsDisplay.setMap(null);
+		directionsDisplay = null;
+	}
+	directionsDisplay = new google.maps.DirectionsRenderer();
+	directionsDisplay.setMap(map);
+
+	if (place.geometry.viewport) {
+		map.fitBounds(place.geometry.viewport);
+	} else {
+		map.setCenter(place.geometry.location);
+		map.setZoom(15);
+	}
+	
+	marker.setPosition(place.geometry.location);
+	marker.setVisible(true);
+
+	// Display the user input location info window
+	var address = '';
+	if (place.address_components) {
+		address = [
+		(place.address_components[0] && place.address_components[0].short_name || ''), 
+		(place.address_components[1] && place.address_components[1].short_name || ''), 
+		(place.address_components[2] && place.address_components[2].short_name || '')
+		].join(' ');
+	}
+	infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + address);
+	infowindow.open(map, marker);
+
+	// Found food trucks around you
+	setMarkers(infowindow);
+}
 
 /*************************************************************/
 // Calculate route between the user location 
@@ -137,6 +203,7 @@ function calcRoute() {
 function setRange() {
 	range = document.getElementById("range").value;
 	//console.log("setRange: " + range);
+	rangeUpdated();
 }
 
 /*************************************************************/
@@ -160,8 +227,10 @@ function clearMarkers() {
 	selectedMarkerId = -1;
 	if (markersArray.length != 0) {
 		for (var i = 0; i < markersArray.length; i++) {
-			if (typeof markersArray[i] == 'object') 
+			if (typeof markersArray[i] == 'object') {
+				markersArray[i].setVisible(false);
 				markersArray[i].setMap(null);
+			}
 		}
 		markersArray.length = 0;
 	}
